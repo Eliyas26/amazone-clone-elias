d6787a2de55c3721d4af3a8f018c477b04134d38
Eliyas26 <119291900+Eliyas26@users.noreply.github.com>
2023-10-03 22:52:14 +0300

    new update

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  // baseURL: "http://127.0.0.1:5001/e-clone-7e5a1/us-central1/api",
-  baseURL: "http://127.0.0.1:5500",
+  baseURL: "https://amazone-clone.cyclic.cloud",
 });
 
 export default instance;
